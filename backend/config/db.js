8552364cdb1a5ebbfcf8dbe830b7af1668abf96c
Davi Silva Santos <morg.daviss@gmail.com>
2022-12-12 22:35:15 -0300

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,8 @@ const dbPassword = process.env.DB_PASS;
 
 const conn = async () => {
     try {
-        const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.edfqecd.mongodb.net/?retryWrites=true&w=majority`)
+        const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.zszri1u.mongodb.net/myFirstDatabase`)
+        // const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.edfqecd.mongodb.net/?retryWrites=true&w=majority`)
 
         console.log("conectou ao banco");
 
