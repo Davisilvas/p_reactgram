959f3c43f3b74ffadecc8e04203b4c72dde141d2
Davi Silva Santos <morg.daviss@gmail.com>
2022-12-13 22:17:54 -0300

    iniciando o front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,20 +8,29 @@ import {
   Navigate
 } from "react-router-dom"
 
+// *** components
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
 // *** Pages
 import Home from './pages/Home/Home';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/login' element={<Login />}/>
-          <Route path='/register' element={<Register />}/>
-        </Routes>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='/register' element={<Register />}/>
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
